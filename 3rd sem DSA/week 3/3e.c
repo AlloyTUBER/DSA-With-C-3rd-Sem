15d6f255603bac22853150a2d205e73e6c5227d1
Rishin Mukherjee <115855697+AlloyTUBER@users.noreply.github.com>
2025-08-14 19:37:53 +0530

    Update 3e.c

--- 3rd sem DSA/week 3/3e.c
+++ 3rd sem DSA/week 3/3e.c
@@ -6,26 +6,6 @@ struct Node {
     struct Node *next;
 };
 
-struct Node* create(int n) {
-    struct Node *head = NULL, *temp = NULL, *new_Node = NULL;
-    float value;
-    for(int i = 0; i < n; i++) {
-        printf("Enter float value for node %d: ", i+1);
-        scanf("%f", &value);
-        new_Node = (struct Node*)malloc(sizeof(struct Node));
-        new_Node->data = value;
-        new_Node->next = NULL;
-        if(head == NULL) {
-            head = new_Node;
-            temp = new_Node;
-        } else {
-            temp->next = new_Node;
-            temp = new_Node;
-        }
-    }
-    return head;
-}
-
 void display(struct Node *head) {
     struct Node *temp = head;
     printf("Linked List: ");
@@ -53,19 +33,27 @@ void sum_and_mean(struct Node *head) {
 }
 
 int main() {
-    int n;
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
+    struct Node head, node1, node2, node3;
 
-    struct Node *head = create(n);
-    display(head);
-    sum_and_mean(head);
+    head.next = &node1;
+    
+    node1.data = 10.3;
+    node1.next = &node2;
+
+    node2.data = 27.9;
+    node2.next = &node3;
+
+    node3.data = 33.4;
+    node3.next = NULL;
+
+    display(head.next);
+    sum_and_mean(head.next);
 
     // Free memory
     struct Node *temp;
-    while(head != NULL) {
-        temp = head;
-        head = head->next;
+    while(head.next != NULL) {
+        temp = head.next;
+        head.next = head.next->next;
         free(temp);
     }
     return 0;
